c0d8925c0ccbbd59fe19f08ec0ffbd041fd457de
devGusta2 <gustavo.souza2979@gmail.com>
2024-03-21 18:49:54 -0300

    SQL-INJECTION

--- SQL-INJECTION/index.php
+++ SQL-INJECTION/index.php
@@ -3,17 +3,29 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
     <title>Document</title>
 </head>
     <body>
-        <div class="login-box">
-            <form action="verify.php" method="post">
-                <label>E-mail:</label>
-                <input type="text" name="email">
-                <label>Senha:</label>
-                <input type="text" name="senha">
-                <input type="submit" value="Entrar">
-            </form>
+        <div class="container">
+            <div class="cad-box">
+                    <h2>Se não possui uma contra, crie agora mesmo!</h2>
+
+                    <div class="button-cad">
+                        <a href=""><h4>Cadastre-se</h4></a>
+                    </div>
+            </div>
+            <div class="login-box">
+                <form action="verify.php" method="post">
+                <h2>Bem-vindo de volta!</h2>
+                    <label>E-mail:</label>
+                    <input type="text" name="email" class="inpt">
+                    <label>Senha:</label>
+                    <input type="text" name="senha" class="inpt">
+                    <button type="submit" class="button"><h4>Entrar</h4></button>
+                
+                </form>
+            </div>
         </div>
     </body>
 </html>
