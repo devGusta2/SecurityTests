f955ef9525d5a2852b668b905b8bbd91eafd3394
devGusta2 <gustavo.souza2979@gmail.com>
2024-03-21 16:48:00 -0300

    SQL-INJECTION

--- SQL-INJECTION/verify.php
+++ SQL-INJECTION/verify.php
@@ -5,13 +5,17 @@
     $senha=$_POST['senha'];
 
     $select="SELECT * FROM tbUser WHERE emailUser='$email' AND senhaUser='$senha'";
-    $select=$mysqli->query($select);
-    $rows=$select->num_rows;
-    while($res=mysqli_fetch_assoc($select)){
+    $result=$mysqli->prepare($select);
+    $result->execute();
+    $rows=$result->num_rows;
+
+    while($res=mysqli_fetch_assoc($result)){
         $sb=$res['sbNomeUser'];
         $senhaBanco=$res['senhaUser'];
         if($rows){
+            echo "SobreNOME:";
             echo $sb;
+            echo "<br>";
         }
     }
  
