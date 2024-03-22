63a7deea66caa84112edc291f6c1b8e0afac0912
devGusta2 <gustavo.souza2979@gmail.com>
2024-03-21 22:43:15 -0300

    SLQ-INJECTION

--- SQL-INJECTION/verify.php
+++ SQL-INJECTION/verify.php
@@ -4,20 +4,22 @@
     $email=$_POST['email'];
     $senha=$_POST['senha'];
 
-    // $select="SELECT * FROM tbUser WHERE emailUser='$email' AND senhaUser='$senha'";
-    // $result=$mysqli->query($select);
+     $select="SELECT * FROM tbUser WHERE senhaUser='$senha' AND emailUser='$email' ";
+     $result=$mysqli->query($select);
 
-    // while($res=mysqli_fetch_assoc($result)){
+        while($res=mysqli_fetch_assoc($result)){
+            echo $res['nameUser'];
+    }
+
+    
+    // $select="SELECT * FROM tbUser WHERE emailUser=? AND senhaUser=?";
+    // $stmt=$mysqli->prepare($select);
+    // $stmt->bind_param('ss',$email,$senha);
+    // $stmt->execute();
+
+    // $result=$stmt->get_result();
+    // while($res=$result->fetch_assoc()){
     //     echo $res['nameUser'];
     // }
-    $select="SELECT * FROM tbUser WHERE emailUser=? AND senhaUser=?";
-    $stmt=$mysqli->prepare($select);
-    $stmt->bind_param('ss',$email,$senha);
-    $stmt->execute();
-
-    $result=$stmt->get_result();
-    while($res=$result->fetch_assoc()){
-        echo $res['nameUser'];
-    }
 
 ?>
