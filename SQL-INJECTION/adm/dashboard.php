a4a625b2380b69122318693c4db012fd6d5f1086
devGusta2 <gustavo.souza2979@gmail.com>
2024-03-24 19:00:03 -0300

    SQL-INJECTION

---
+++ SQL-INJECTION/adm/dashboard.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/style-adm.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <div class="menu">
+            <img src="../img/user-icon1.png" alt="">
+            <ul>
+                <li><a href="" class="link"><i class="fa-solid fa-house fa-2x"></i><p>Dashboard</p></a></li>
+                <li><a href="users.php" class="link"><i class="fa-solid fa-user  fa-2x"></i><p>Users</p></a></li>
+                <li><a href="" class="link"><i class="fa-solid fa-file fa-2x"></i><p>Pages</p></a></li>
+                <li><a href="" class="link"><i class="fa-solid fa-envelope fa-2x"></i><p>E-mails</p></a></li>
+                <li><a href="" class="link"><i class="fa-solid fa-phone fa-2x"></i><p>Contact</p></a></li>
+                <li><a href="" class="link"><i class="fa-solid fa-comment-dots fa-2x"></i><p>Comments</p></a></li>
+            </ul>
+            <a href=""><i class="fa-solid fa-right-from-bracket"></i>Sair</a>
+        </div>
+    </div>
+    <script src="https://kit.fontawesome.com/787aeba2d2.js" crossorigin="anonymous"></script>
+</body>
+</html>
