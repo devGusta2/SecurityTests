e35712adb24c3c412bcf02cc837f5b5642e95ec2
devGusta2 <gustavo.souza2979@gmail.com>
2024-03-24 13:34:12 -0300

    SQL-INJECTION

---
+++ SQL-INJECTION/adm/painel.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/adm.css">
+    <title>Admin</title>
+</head>
+<body>
+    <div class="container">
+        <div class="menu">
+            <img src="../img/user-icon.png" alt="">
+ 
+            <a href="">Sair</a>
+        </div>
+    </div>
+    <script src="https://kit.fontawesome.com/787aeba2d2.js" crossorigin="anonymous"></script>
+</body>
+</html>
