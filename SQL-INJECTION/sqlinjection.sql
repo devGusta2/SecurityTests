16842f788f4c29757c94fe15ee2d68ef12daa344
devGusta2 <gustavo.souza2979@gmail.com>
2024-03-24 18:22:31 -0300

    SQL-INJECTION

--- SQL-INJECTION/sqlinjection.sql
+++ SQL-INJECTION/sqlinjection.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 21/03/2024 às 20:14
+-- Tempo de geração: 24/03/2024 às 22:21
 -- Versão do servidor: 10.4.28-MariaDB
--- Versão do PHP: 8.2.4
+-- Versão do PHP: 8.0.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,20 +24,45 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estrutura para tabela `tbadm`
+--
+
+CREATE TABLE `tbadm` (
+  `idAdm` int(11) NOT NULL,
+  `nomeAdm` varchar(20) NOT NULL,
+  `emailAdm` varchar(30) NOT NULL,
+  `senhaAdm` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Estrutura para tabela `tbuser`
 --
 
 CREATE TABLE `tbuser` (
   `idUser` int(11) NOT NULL,
-  `nomeUser` varchar(50) DEFAULT NULL,
-  `emailUser` varchar(30) DEFAULT NULL,
-  `senhaUser` varchar(20) DEFAULT NULL,
-  `sbNomeUser` varchar(30) NOT NULL
+  `nameUser` varchar(50) NOT NULL,
+  `sobrenomeUser` varchar(50) NOT NULL,
+  `emailUser` varchar(100) NOT NULL,
+  `senhaUser` varchar(50) NOT NULL,
+  `telUser` varchar(20) NOT NULL,
+  `dataNascUser` date NOT NULL,
+  `cpfUser` char(11) NOT NULL,
+  `logUser` varchar(200) NOT NULL,
+  `cidadeUser` varchar(30) NOT NULL,
+  `estadoUser` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Índices para tabelas despejadas
 --
+
+--
+-- Índices de tabela `tbadm`
+--
+ALTER TABLE `tbadm`
+  ADD PRIMARY KEY (`idAdm`);
 
 --
 -- Índices de tabela `tbuser`
@@ -48,6 +73,12 @@ ALTER TABLE `tbuser`
 --
 -- AUTO_INCREMENT para tabelas despejadas
 --
+
+--
+-- AUTO_INCREMENT de tabela `tbadm`
+--
+ALTER TABLE `tbadm`
+  MODIFY `idAdm` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de tabela `tbuser`
