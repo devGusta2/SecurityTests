7dd905c4886fce9ed754ca879d4005b2084d69cd
devGusta2 <gustavo.souza2979@gmail.com>
2024-03-25 21:42:40 -0300

    SQL-INJECT

--- SQL-INJECTION/adm/users.php
+++ SQL-INJECTION/adm/users.php
@@ -31,18 +31,8 @@
 </head>
 <body>
 <div class="container">
-<div class="menu">
-            
-            <ul>
-                <li><a href="dashboard.php" class="link"><i class="fa-solid fa-house fa-2x"></i><p>Dashboard</p></a></li>
-                <li><a href="users.php" class="link"><i class="fa-solid fa-user  fa-2x"></i><p>Users</p></a></li>
-                <li><a href="" class="link"><i class="fa-solid fa-file fa-2x"></i><p>Pages</p></a></li>
-                <li><a href="" class="link"><i class="fa-solid fa-envelope fa-2x"></i><p>E-mails</p></a></li>
-                <li><a href="" class="link"><i class="fa-solid fa-phone fa-2x"></i><p>Contact</p></a></li>
-                <li><a href="" class="link"><i class="fa-solid fa-comment-dots fa-2x"></i><p>Comments</p></a></li>
-            </ul>
-            <a href=""><i class="fa-solid fa-right-from-bracket"></i>Sair</a>
-        </div>
+    <div class="menu-vert"></div>
+    <div class="table-box">
     <table>
         <thead>
             <tr>
@@ -85,6 +75,7 @@
             ?>
         </tbody>
     </table>
+    </div>
 </div>
 <script src="https://kit.fontawesome.com/787aeba2d2.js" crossorigin="anonymous"></script>
 </body>
