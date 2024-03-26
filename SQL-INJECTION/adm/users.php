ad882d04fa253d496d29599778cd8b0a3d58d6af
devGusta2 <gustavo.souza2979@gmail.com>
2024-03-26 15:31:23 -0300

    SQL-INJECTION

--- SQL-INJECTION/adm/users.php
+++ SQL-INJECTION/adm/users.php
@@ -4,35 +4,26 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles/style-adm.css">
+    <link rel="stylesheet" href="styles/style-user.css">
     <title>Tabela de Usuários</title>
     <style>
-        table {
-            border-collapse: collapse;
-            width: 80%;
-            margin-left:200px;
-        }
-
-        th, td {
-            border: 1px solid black;
-            padding: 8px;
-            text-align: left;
-        }
-
-        th {
-            background-color: #f2f2f2;
-        }
-        .container{
-            display:flex;
-            flex-direction:row;
-            align-items:center;
-            justify-content:center;
-        }
+       
     </style>
 </head>
 <body>
 <div class="container">
-    <div class="menu-vert"></div>
-    <div class="table-box">
+    <div class="menu-vert">
+        <ul>
+                    <li><a href="dashboard.php" class="link"><i class="fa-solid fa-house fa-2x"></i><p>Dashboard</p></a></li>
+                    <li><a href="users.php" class="link"><i class="fa-solid fa-user  fa-2x"></i><p>Users</p></a></li>
+                    <li><a href="" class="link"><i class="fa-solid fa-file fa-2x"></i><p>Pages</p></a></li>
+                    <li><a href="" class="link"><i class="fa-solid fa-envelope fa-2x"></i><p>E-mails</p></a></li>
+                    <li><a href="" class="link"><i class="fa-solid fa-phone fa-2x"></i><p>Contact</p></a></li>
+                    <li><a href="" class="link"><i class="fa-solid fa-comment-dots fa-2x"></i><p>Comments</p></a></li>
+                </ul>
+                <a href=""><i class="fa-solid fa-right-from-bracket"></i>Sair</a>
+        </div>
+        <div class="table-box">
     <table>
         <thead>
             <tr>
