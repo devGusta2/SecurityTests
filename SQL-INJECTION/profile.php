085410b142fbf9e89696d93b143d35a6ebcd4d81
devGusta2 <gustavo.souza2979@gmail.com>
2024-03-27 19:51:06 -0300

    SQL-INJECT

--- SQL-INJECTION/profile.php
+++ SQL-INJECTION/profile.php
@@ -1,8 +1,20 @@
-<?php  include('protect.php');?>
+<?php
+  include('protect.php');
+  include('conexao.php');
+  $id=$_SESSION['id'];
+  $select="SELECT * FROM tbUser WHERE idUser='$id'";
+    $stmt=$mysqli->query($select);
+
+    while($data=mysqli_fetch_assoc($stmt)){
+        $nome=$data['nameUser'];
+        $sbnome=$data['sobrenomeUser'];
+        $email=$data['emailUser'];
+    }
+?>
 
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -12,6 +24,10 @@
 <body>
     <div class="container">
         <header>
+            <div class="user">
+            <i class="fa-solid fa-user"></i>
+                <?php echo "Bem vindo(a)".$nome;?>
+            </div>
             <ul>
                 <li>
                     <select name="" id="">
@@ -35,7 +51,50 @@
                 </li>
             
             </ul>
+            <i class="fa-solid fa-bag-shopping fa-2x"></i>
+            <i class="fa-solid fa-bars fa-2x"></i>
+            <a href="sair.php"><i class="fa-solid fa-arrow-right-from-bracket"></i></a>
+            
         </header>
+
+
+        <main>
+            <div class="card">
+                <i class="fa-solid fa-user fa-5x"></i>
+                
+               <div class="box">
+                    <div class="info">
+                        <?php
+                            echo $nome." ".$sbnome;
+                        ?>
+                    </div>
+                    <?php
+                        echo $email;
+                    ?>
+               </div>
+          
+            </div>
+            <div class="card">
+                <i class="fa-solid fa-credit-card fa-3x"></i>
+                    <div class="info">
+                       <div class="box">
+                       <h5>Dados pessoais</h5>
+                            <p style="font-size:12pt; font-weight:normal;">Informações do seu documento de identidade.</p>
+                       </div>
+                    </div>
+                
+            </div>
+            <div class="card">
+                <i class="fa-solid fa-lock fa-3x"></i>
+                <div class="info">
+                       <div class="box">
+                            <h5>Segurança</h5>
+                            <p style="font-size:12pt; font-weight:normal;">Aumente a segurança de sua conta!</p>
+                       </div>
+                    </div>
+            </div>
+        </main>
     </div>
+    <script src="https://kit.fontawesome.com/787aeba2d2.js" crossorigin="anonymous"></script>
 </body>
 </html>
