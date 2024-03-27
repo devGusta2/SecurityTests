6744a84320b82de141f8bf4fa632e5f025250d98
devGusta2 <gustavo.souza2979@gmail.com>
2024-03-27 19:10:45 -0300

    SQL-INJECT

--- SQL-INJECTION/profile.php
+++ SQL-INJECTION/profile.php
@@ -1,3 +1,41 @@
 <?php  include('protect.php');?>
-<h3>Logado >:D</h3>
-<a href="sair.php">Sair</a>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/profile.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <header>
+            <ul>
+                <li>
+                    <select name="" id="">
+                        <option value="">Categorias</option>
+                    </select>
+                </li>
+                <li>
+                    <select name="" id="">
+                        <option value="">Ofertas</option>
+                    </select>
+                </li>
+                <li>
+                    <select name="" id="">
+                        <option value="">Periféricos</option>
+                    </select>
+                </li>
+                <li>
+                    <select name="" id="">
+                        <option value="">Computadores</option>
+                    </select>
+                </li>
+            
+            </ul>
+        </header>
+    </div>
+</body>
+</html>
